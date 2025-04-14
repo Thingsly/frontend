ea249bc16f870cf6e3c11326674d64207daa48bf
hantdev <captainnemot1k60@gmail.com>
2025-04-14 07:46:44 +0700

    feat: update .gitignore and improve TypeScript configurations for better environment handling

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -1770,8 +1770,8 @@ declare namespace App {
 
     type GetI18nKey<T extends Record<string, unknown>, K extends keyof T = keyof T> = K extends string
       ? T[K] extends Record<string, unknown>
-      ? `${K}.${GetI18nKey<T[K]>}`
-      : K
+        ? `${K}.${GetI18nKey<T[K]>}`
+        : K
       : never;
 
     type I18nKey = GetI18nKey<Schema>;
@@ -1901,12 +1901,12 @@ declare namespace App {
     /** 多个请求数据结果 */
     type MultiRequestResult<T extends any[]> = T extends [infer First, ...infer Rest]
       ? [First] extends [any]
-      ? Rest extends any[]
-      ? [Service.RequestResult<First>, ...MultiRequestResult<Rest>]
-      : [Service.RequestResult<First>]
-      : Rest extends any[]
-      ? MultiRequestResult<Rest>
-      : []
+        ? Rest extends any[]
+          ? [Service.RequestResult<First>, ...MultiRequestResult<Rest>]
+          : [Service.RequestResult<First>]
+        : Rest extends any[]
+          ? MultiRequestResult<Rest>
+          : []
       : [];
 
     /** 请求结果的适配器函数 */
