9287a7f157c82c2d47a638a003299545403cdc40
hantdev <captainnemot1k60@gmail.com>
2025-04-19 18:31:42 +0700

    feat(components): add various configuration records and options for theme, user roles, and service management

--- src/utils/form/rule.ts
+++ src/utils/form/rule.ts
@@ -2,24 +2,17 @@ import type { Ref } from 'vue';
 import type { FormItemRule } from 'naive-ui';
 import { $t } from '@/locales';
 import { REG_CODE_SIX, REG_EMAIL, REG_PHONE, REG_PWD } from '@/constants/reg';
-/** 创建自定义错误信息的必填表单规则 */
 export const createRequiredFormRule = (message = $t('form.required')): FormItemRule => ({ required: true, message });
 
 export const requiredFormRule = createRequiredFormRule();
 
-/** 表单规则 */
 interface CustomFormRules {
-  /** 手机号码 */
   phone: FormItemRule[];
-  /** 密码 */
   pwd: FormItemRule[];
-  /** 验证码 */
   code: FormItemRule[];
-  /** 邮箱 */
   email: FormItemRule[];
 }
 
-/** 表单规则 */
 export const formRules: CustomFormRules = {
   phone: [
     createRequiredFormRule($t('form.phone.required')),
@@ -47,12 +40,10 @@ export const formRules: CustomFormRules = {
   ]
 };
 
-/** 是否为空字符串 */
 function isBlankString(str: string) {
   return str.trim() === '';
 }
 
-/** 获取确认密码的表单规则 */
 export function getConfirmPwdRule(pwd: Ref<string>) {
   const confirmPwdRule: FormItemRule[] = [
     { required: true, message: $t('form.pwd.tip') },
@@ -70,7 +61,6 @@ export function getConfirmPwdRule(pwd: Ref<string>) {
   return confirmPwdRule;
 }
 
-/** 获取图片验证码的表单规则 */
 export function getImgCodeRule(imgCode: Ref<string>) {
   const imgCodeRule: FormItemRule[] = [
     { required: true, message: $t('form.code.required') },
