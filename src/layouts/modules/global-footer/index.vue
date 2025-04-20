23140f2dd685ba09f89569e315bd6793b3519094
hantdev <captainnemot1k60@gmail.com>
2025-04-20 22:24:17 +0700

    feat(other): implement global menu and theme drawer components

---
+++ src/layouts/modules/global-footer/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'GlobalFooter'
+});
+</script>
+
+<template>
+  <DarkModeContainer class="h-full flex-center">
+    <a
+      href="https://github.com/Thingsly"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Copyright © 2025 Thingsly IoT Platform
+    </a>
+  </DarkModeContainer>
+</template>
+
+<style scoped></style>
