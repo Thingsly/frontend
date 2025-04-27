dd4a2fdd7f0bb8166e1cc70d7421f6702ad2ab37
hantdev <captainnemot1k60@gmail.com>
2025-04-27 21:02:15 +0700

    fix(components): hot fix

--- src/components/panel/ui/card-form.vue
+++ src/components/panel/ui/card-form.vue
@@ -56,7 +56,7 @@ const dataAggregateRangeOptions = [
   { label: $t('common.hours6'), value: '6h', disabled: false },
   { label: $t('common.days1'), value: '1d', disabled: false },
   { label: $t('common.days7'), value: '7d', disabled: false },
-  { label: '1月', value: '1mo', disabled: false }
+  { label: '1 month', value: '1mo', disabled: false }
 ];
 const aggregateFunctionOptions: SelectOption[] = [
   { label: $t('common.average'), value: 'avg' },
