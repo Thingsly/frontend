eb8f2567bb51b170002dc7c55c426a002351dddb
hantdev <captainnemot1k60@gmail.com>
2025-05-17 00:28:09 +0700

    chore(components): update environment variable files to be ignored consistently

---
+++ env.config.ts
@@ -0,0 +1,56 @@
+/**
+ * Create service config by current env
+ *
+ * @param env The current env
+ */
+export function createServiceConfig(env: Env.ImportMeta) {
+  const mockURL = 'http://127.0.0.1:9999/api/v1';
+  const devURL = 'http://127.0.0.1:9999/api/v1';
+  // const devURL = '  http://127.0.0.1:8080/api/v1';
+  // const devURL = 'http://mitras.cloud/api/v1';
+  // const devURL = 'http://demo.mitras.cloud/api/v1';
+
+  const testURL = '';
+  const prodURL = '';
+
+  const serviceConfigMap: App.Service.ServiceConfigMap = {
+    dev: {
+      baseURL: devURL,
+      otherBaseURL: {
+        demo: devURL,
+        mock: mockURL
+      }
+    },
+    test: {
+      baseURL: mockURL,
+      otherBaseURL: {
+        demo: testURL,
+        mock: mockURL
+      }
+    },
+    prod: {
+      baseURL: prodURL,
+      otherBaseURL: {
+        demo: prodURL,
+        mock: mockURL
+      }
+    }
+  };
+
+  const { VITE_SERVICE_ENV = 'dev' } = env;
+
+  return serviceConfigMap[VITE_SERVICE_ENV];
+}
+
+/**
+ * Get proxy pattern of service url
+ *
+ * @param key If not set, will use the default key
+ */
+export function createProxyPattern(key?: App.Service.OtherBaseURLKey) {
+  if (!key) {
+    return '/proxy-default';
+  }
+
+  return `/proxy-${key}`;
+}
